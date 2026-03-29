d650a888ed56e46ddc6e7ffab44d1c2db5fbb8c9
shashisrs200 <shashisrs.200@gmail.com>
2026-03-29 12:38:28 +0530

    Completed the CRUD operation with the database using Mongoose

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,11 +1,150 @@
 import productModel from "../models/productModel.js";
+
+//Create One Product
 const createProduct = async (req, res) => {
   try {
     let data = req.body;
     let product = await productModel.create(data);
     return res.send({ message: "Ok", data: product });
   } catch (error) {
+    console.log(error);
     return res.send({ message: "Failed" });
   }
 };
-export { createProduct };
+
+// Create many Products
+const createProducts = async (req, res) => {
+  try {
+    let data = req.body;
+    let products = await productModel.insertMany(data);
+    return res.send({ message: "Ok", data: products });
+  } catch (error) {
+    console.log(error);
+    return res.send({ message: "Failed" });
+  }
+};
+
+// Get all the products
+const getProducts = async (req, res) => {
+  try {
+    let products = await productModel.find();
+    return res.send({ message: "Ok", data: products });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      message: "Failed to fetch products",
+      error: error.message,
+    });
+  }
+};
+
+// Get the specific product by id
+const getProductById = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let product = await productModel.findById(id);
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({
+      message: "Failed to fetch products",
+      error: error.message,
+    });
+  }
+};
+
+// Get the specific product by title
+const getProductByTitle = async (req, res) => {
+  try {
+    let { title } = req.params;
+    let product = await productModel.findOne({ title: title });
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({
+      message: "Failed to fetch products",
+      error: error.message,
+    });
+  }
+};
+
+// update one product
+const updateProduct = async (req, res) => {
+  try {
+    let { productId, title, price } = req.body;
+    let product = await productModel.updateOne(
+      { productId },
+      { $set: { title, price } },
+    );
+
+    // It can also be written like this
+    // let product=await productModel.updateOne({productId:productId},{$set:{title:title,price:price}});
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({ meassage: "Ok", error: error.message });
+  }
+};
+
+// update many products at a time
+const updateProducts = async (req, res) => {
+  try {
+    let { author, category } = req.body;
+    let product = await productModel.updateMany(
+      { author: author },
+      { $set: { category: category } },
+    );
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({ message: "Ok", error: error.messsage });
+  }
+};
+
+// update the specific product by id
+const updateProductById = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let data = req.body;
+    let product = await productModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: data },
+    );
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({ messsage: "Ok", error: error.message });
+  }
+};
+
+// delete the product -settting isDelete to true and then showing the only data which is isDelete=false according to our situations
+const deleteProduct = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let product = await productModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: { isDelete: true } },
+    );
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({ message: "Ok", error: error.message });
+  }
+};
+
+// Permanently deleteing data from the database
+const pDelete = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let product = await productModel.findByIdAndDelete({ _id: id });
+    return res.send({ message: "Ok", data: product });
+  } catch (error) {
+    return res.send({ message: "Ok", error: error.message });
+  }
+};
+export {
+  createProduct,
+  createProducts,
+  getProducts,
+  getProductById,
+  getProductByTitle,
+  updateProduct,
+  updateProducts,
+  updateProductById,
+  deleteProduct,
+  pDelete,
+};
--- src/route.js
+++ src/route.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { createProduct } from "./controllers/productController.js";
+import { createProduct, createProducts,getProducts,getProductById, getProductByTitle,updateProduct, updateProducts, updateProductById, deleteProduct, pDelete} from "./controllers/productController.js";
 const router=express.Router();
 router.get("/api",(req,res)=>{
     return res.send({message:"Ok"})
 });
-router.post("/addproduct",createProduct)
+router.post("/addproduct",createProduct);
+router.post("/addproducts",createProducts);
+router.get("/allproducts",getProducts);
+router.get("/products/:id",getProductById);
+router.get("/products/by-title/:title",getProductByTitle);
+router.patch("/updateproduct",updateProduct);
+router.patch("/updateproducts",updateProducts);
+router.patch("/updateproductbyid/:id",updateProductById);
+router.delete("/deleteproduct/:id",deleteProduct);
+router.delete("/permanentdelete/:id",pDelete)
 export default router;
